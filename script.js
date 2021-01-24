1e1586f33fb57c128a1f362c282a8aae89cf47ee
Sam Kim <sendtosamkim@gmail.com>
2021-01-23 21:23:04 -0500

    final work with readme.md complete week 2

--- script.js
+++ script.js
@@ -1,4 +1,93 @@
-// Assignment Code
+
+// password content arrays
+var array1 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+
+const small = 'abcdefghijklmnopqrstuvwxyz';
+var array2 = Array.from(small);
+
+const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var array3 = Array.from(upper);
+
+var array4 = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "_", "+", "`", "~"];
+
+// the questions for User
+function getOptions() {
+  var charLength = parseInt(prompt("how many characters do you want? Choose from 8~128."));
+  if (charLength < 8 || charLength > 128 || !isNaN(length) === false) {
+    alert("The character length has to be between 8 and 128!");
+    return;
+  }
+  var question1 = confirm("Do you want upper characters?");
+  var question2 = confirm("Do you want lower characters?");
+  var question3 = confirm("Do you want special characters?");
+  var question4 = confirm("do you want number?");
+
+  if (
+    question1 === false &&
+    question2 === false &&
+    question3 === false &&
+    question4 === false) {
+    alert('It looks like you don\'t want a password.');
+    return;
+  }
+  var myAnswerObject = {
+    length: charLength,
+    upperChar: question1,
+    lowerChar: question2,
+    specialChar: question3,
+    nuM: question4,
+  }
+  return myAnswerObject;
+
+}
+
+// generate password function
+function generatePassword() {
+
+  var userAnswers = getOptions();
+  var password = [];
+
+  var array5 = [];
+  console.log(userAnswers);
+
+  if (userAnswers.nuM) {
+    array5 = array5.concat(array1);
+    console.log(array5);
+  }
+  if (userAnswers.upperChar) {
+    array5 = array5.concat(array3);
+    console.log(array5);
+  }
+  if (userAnswers.lowerChar) {
+    array5 = array5.concat(array2);
+    console.log(array5);
+  }
+
+  if (userAnswers.specialChar) {
+    array5 = array5.concat(array4);
+    console.log(array5);
+  }
+
+
+
+
+  // fill up the password up to the length requested by User
+
+  for (var i = 0; i < userAnswers.length; i++) {
+    var randIndex = Math.floor(Math.random() * array5.length);
+    var charChosen = array5[randIndex];
+
+    password.push(charChosen);
+    console.log(password);
+  }
+  return password.join('');
+
+
+}
+
+
+// Add event listener to generate button
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -9,80 +98,4 @@ function writePassword() {
   passwordText.value = password;
 }
 
-
-// crete generate password function
-function generatePassword() {
-
-  var array1 = [1,2,3,4,5,6,7,8,9,0];
-  const small = 'abcdefghijklmnopqrstuvwxyz';
-  var array2 = Array.from(small);
-  console.log(array2);
-  const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  var array3 = Array.from(upper);
-  console.log(array3);
-  var array4 = ["!","@","#","$","%","^","&","*","(",")","-","=","_","+","`","~"];
-  console.log(array4);
-  var array5 =[];
-
-  var charLength = parseInt(prompt("how many characters do you want?"));
-
-  var question1 = confirm("Do you want upper characters?");
-  var question2 = confirm("Do you want lower characters?");
-  var question3 = confirm ("Do you want special characters?");
-  var question4 = confirm("do you want number?");
-
-  var myAnswerObject = {
-    length: charLength,
-    upperChar: question1,
-    lowerChar: question2,
-    specialChar: question3,
-    number: question4,
-  }
-
-
-
-  if( charLength < 8 || charLength > 128) {
-     alert("The character length has to be between 8 and 128!");
-  }
-  if(myAnswerObject.upperChar===true){
-      array5 = array5.concat(array3);
-    }
-    else if(myAnswerObject.lowerChar===true){
-      array5=array5.concat(array2);
-    }
-    else if(myAnswerObject.specialChar===true){
-      array5=array5.concat(array4);
-    }
-    else(myAnswerObject.number===true){
-      array5=array5.concat(array1);
-    }
-
-
-  for ( var i = 0; i < charLength; i++){
-    var index = Math.floor(Math.random()* charLength + 1);
-    var charChosen = array5[index];
-    password += charChosen;
-  }
-    return password;
-
-  }
-
-
-
-  //   for (var i = ); i< charLength; i++){
-  //     var index = Math.floor(Math.random() * charLength + 1);
-  //     var charChosen1 = array3[index];
-  //     var charChosen2 = array2[index];
-  //     password += charChosen1 + charChosen2;
-  //   }
-  // }
-  // function 
-
-
-}
-
-
-
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
